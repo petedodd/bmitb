d90104fcbc339465f0cc61ce933abcbf2294083e
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2025-06-26 21:36:42 +0100

    lopoff work

--- R/2_RRcalculations.R
+++ R/2_RRcalculations.R
@@ -75,6 +75,13 @@ mean(exp(BL(bmi1,t1,t2))) / mean(exp(BL(bmi0,t1,t2)))
 RRfunBL(24,0.7,t1,t2) ## OK
 
 
+## correct magnitude of change?
+mean(bmi1)
+mean(bmi0)
+exp(t*(mean(bmi1)-mean(bmi0)))
+RRfun(24,0.7,t) #about as good as one might expect
+
+
 
 ## example dists: exaggerated
 png(here("output/eg_dist.png"))
@@ -95,15 +102,24 @@ ggplot(bmiz,aes(x=BMI,y=after_stat(density),fill=population))+geom_histogram(alp
 
 ggsave(file=here("output/eg_tb_vs_pop.png"),w=6,h=5)
 
-## correct magnitude of change?
-mean(bmi1)
-mean(bmi0)
-exp(t*(mean(bmi1)-mean(bmi0)))
-RRfun(24,0.7,t) #about as good as one might expect
+## example of "lop-off"
 
-## apply to data
-DRB <- DRB[age!="18-19",.(iso3,Year,Sex,age,k,theta)] #restrict
-DRB[,RR:=RRfun(k,theta,t)]
+## example dists: exaggerated
+png(here("output/eg_lopoff.png"))
+
+
+## truncated renormalized
+lopoff17 <- function(x) ifelse(x<17,0,
+                               dgamma(x,shape=bmirefpop$k,scale=bmirefpop$theta)/
+                               (1-pgamma(17,shape=bmirefpop$k,scale=bmirefpop$theta)))
+curve(lopoff17(x),from=10,to=45,n=1e3,col=2,,main="",xlab="BMI",ylab="")
+abline(v=17,col=2,lty=3)
+curve(dgamma(x,shape=bmirefpop$k,scale=bmirefpop$theta),n=1e3,col=1,add=TRUE)
+
+
+dev.off()
+
+
 
 ## merge against TB estimates
 
@@ -136,6 +152,10 @@ DRB <- merge(DRB[age!="20-24"], #NOTE only consider 25+ for now TODO
              by=c("iso3","Sex","age"))
 
 DRB <- merge(DRB,whokey,by="iso3") #WHO regions
+
+## apply to data
+DRB <- DRB[age!="18-19",.(iso3,Year,Sex,age,k,theta)] #restrict
+DRB[,RR:=RRfun(k,theta,t)]
 
 ## === sense check
 DRB[,mnbmi:=k*theta] #for each country year
@@ -209,3 +229,182 @@ ggplot(RRbyAS,aes(age,RR,fill=Sex)) +
 ggsave(here("output/RR_age_sex.png"),w=7,h=5)
 
 
+## ============ lopoff work
+
+## RRfunBL1 <- function(k,theta,t1,t2){
+##   x0 <- 25; a1 <- -t1; a2 <- -t2;
+##   exp(a1*x0) * pgamma(x0,k,scale=theta/(1+a1*theta)) / (1+a1*theta)^k +
+##     exp(a2*x0) * pgamma(x0,k,scale=theta/(1+a2*theta),lower.tail=FALSE) / (1+a2*theta)^k
+## }
+
+
+
+## RRfunBL2 <- function(k,theta,t1,t2) RRfunBL1(k,theta,t1,t2) /
+##                                       RRfunBL1(bmirefpop$k,bmirefpop$theta,t1,t2)
+
+## mean(rgamma(1e4,2,scale=15))
+## mean(exp(BL(rgamma(1e4,2,scale=15),t1,t1)))
+## RRfunBL1(2,15,t1,t1)
+
+
+## kk <- 2    #k
+## tt <- 15   #theta
+## rr <- 0.1  #rho
+## xx <- rgamma(1e4,kk,scale=tt)
+## mean(xx)
+## mean(exp(-xx*rr))
+## ff <- function(x) exp(-rr*x) * exp(-x/tt) * x^(kk-1) / (gamma(kk)*tt^kk)
+## integrate(ff,lower=0,upper=Inf) #OK
+## 1/(1+rr*tt)^(kk) #OK
+
+
+## mean(exp(BL(bmi1,t1,t1))) / mean(exp(BL(bmi0,t1,t1)))
+## mean(exp(t1 * (bmi1-30))) / mean(exp(t1 * (bmi0-30))) #E_1[exp(t*X)] / E_0[exp(t*X)] with a shift for numerics
+
+## RRfunBL0(24,0.7,t1,t1) / RRfunBL0(bmirefpop$k,bmirefpop$theta,t1,t1)
+## RRfunBL(24,0.7,t1,t1)
+## RRfunBL1(24,0.7,t1,t1) / RRfunBL1(bmirefpop$k,bmirefpop$theta,t1,t1)
+## RRfunBL2(24,0.7,t1,t1)
+
+## ## 18.0% (95%CI: 16.4-19.6) for BMI<25.0kg/m2 and 6.9% (95%CI: 4.6-9.2) for BMI>=25.0kg/m2 in
+## t1 <- -log(1.18) #risk function parameter
+## exp(-t1)          #risk increase with 1 unit decreast
+## t2 <- -log(1.069) #risk function parameter
+## exp(-t2)          #risk increase with 1 unit decreast
+
+## mean(exp(BL(bmi1,t1,t2))) / mean(exp(BL(bmi0,t1,t2)))
+## RRfunBL(24,0.7,t1,t2) ## OK
+## RRfunBL2(24,0.7,t1,t2) ## OK
+
+RRlopoff0 <- function(k,theta,t1,t2,L){
+  x0 <- 25; a1 <- -t1; a2 <- -t2;
+  ans <- exp(a1*x0) * (pgamma(x0,k,scale=theta/(1+a1*theta)) -
+                       pgamma(L,k,scale=theta/(1+a1*theta))) / (1+a1*theta)^k +
+         exp(a2*x0) * pgamma(x0,k,scale=theta/(1+a2*theta),lower.tail=FALSE) / (1+a2*theta)^k
+  ans / pgamma(L,k,scale=theta,lower.tail=FALSE)
+}
+
+## ## test
+## RRfunBL1(24,0.7,t1,t1)
+## RRlopoff0(24,0.7,t1,t1,17)
+
+
+RRlopoff <- function(k,theta,t1,t2,L) RRlopoff0(k,theta,t1,t2,L) /
+                                        RRlopoff0(bmirefpop$k,bmirefpop$theta,t1,t2,0)
+
+## ## test
+## RRfunBL2(24,0.7,t1,t1)
+## RRlopoff(24,0.7,t1,t1,0)
+## RRlopoff(24,0.7,t1,t1,17)
+
+## compute values:
+DRB[,RR0:=RRlopoff(k,theta,t1,t2,0)]
+DRB[,RR17:=RRlopoff(k,theta,t1,t2,17)]
+DRB[,RR18.5:=RRlopoff(k,theta,t1,t2,18.5)]
+
+## ## over time
+## RRbyT <- DRB[,.(RR0=weighted.mean(RR0,tb),
+##                 RR17=weighted.mean(RR17,tb),
+##                 RR18.5=weighted.mean(RR18.5,tb)
+##                 ),by=Year]
+
+
+## ## over time & by country
+## RRbyTC <- DRB[,.(RR=weighted.mean(RR,tb)),by=.(Year,iso3)]
+## RRbyTC <- merge(RRbyTC,TBT[,.(iso3,Year=year,e_inc_100k,g_whoregion)],by=c("Year","iso3"))
+## RRbyTC[,hi:=mean(e_inc_100k)>100,by=iso3]
+## RRbyTC[,mn:=mean(e_inc_100k),by=iso3]
+## RRbyTC[Year==2022,lbl:=iso3]
+
+## by age and sex
+RRbyAS <- DRB[Year==2022,.(RR0=weighted.mean(RR0,tb),
+                 RR17=weighted.mean(RR17,tb),
+                 RR18.5=weighted.mean(RR18.5,tb)),by=.(Sex,age)]
+RRbyAS[,RR17:=RR17/RR0]
+RRbyAS[,RR18.5:=RR18.5/RR0]
+
+RRbyAS <- melt(RRbyAS[,.(Sex,age,RR17,RR18.5)],id=c("Sex","age"))
+
+## by age and sex
+ggplot(RRbyAS,aes(age,value,fill=variable)) +
+  geom_bar(stat="identity",position="dodge")+
+  theme_linedraw()+
+  expand_limits(y=c(0,NA))+
+  facet_wrap(~Sex)+
+  ylab("Global weighted risk ratio")+
+  theme(legend.position = "top")
+
+ggsave(here("output/RR_age_sex_lopoff.png"),w=12,h=5)
+
+
+
+## by age and sex
+RRbyASR <- DRB[Year==2022,.(RR0=weighted.mean(RR0,tb),
+                           RR17=weighted.mean(RR17,tb),
+                           RR18.5=weighted.mean(RR18.5,tb)),by=.(Sex,age,g_whoregion)]
+RRbyASR[,RR17:=RR17/RR0]
+RRbyASR[,RR18.5:=RR18.5/RR0]
+RRbyASR <- melt(RRbyASR[,.(Sex,age,g_whoregion,RR17,RR18.5)],id=c("Sex","age","g_whoregion"))
+
+## by age and sex
+ggplot(RRbyASR,aes(age,value,fill=variable)) +
+  geom_bar(stat="identity",position="dodge")+
+  theme_linedraw()+
+  expand_limits(y=c(0,NA))+
+  facet_grid(g_whoregion ~ Sex)+
+  ylab("Global weighted risk ratio")+
+  theme(legend.position = "top")
+
+ggsave(here("output/RR_age_sex_reg_lopoff.png"),w=12,h=15)
+
+
+## by region and sex
+RRbySR <- DRB[Year==2022,.(RR0=weighted.mean(RR0,tb),
+                           RR17=weighted.mean(RR17,tb),
+                           RR18.5=weighted.mean(RR18.5,tb)),by=.(Sex,g_whoregion)]
+RRbySR[,RR17:=RR17/RR0]
+RRbySR[,RR18.5:=RR18.5/RR0]
+RRbySR <- melt(RRbySR[,.(Sex,g_whoregion,RR17,RR18.5)],id=c("Sex","g_whoregion"))
+
+## sex globall
+RRbySG <- DRB[Year==2022,.(RR0=weighted.mean(RR0,tb),
+                           RR17=weighted.mean(RR17,tb),
+                           RR18.5=weighted.mean(RR18.5,tb)),by=.(Sex)]
+RRbySG[,RR17:=RR17/RR0]
+RRbySG[,RR18.5:=RR18.5/RR0]
+RRbySG <- melt(RRbySG[,.(Sex,RR17,RR18.5)],id=c("Sex"))
+RRbySG[,g_whoregion:=NA]
+
+## by region and sex
+ggplot(RRbySR,aes(g_whoregion,value,fill=variable)) +
+  geom_bar(stat="identity",position="dodge")+
+  geom_hline(data=RRbySG,aes(yintercept=value,lty=variable),col=2)+
+  theme_linedraw()+
+  expand_limits(y=c(0,NA))+
+  facet_grid(~ Sex)+
+  ylab("Global weighted risk ratio")+
+  theme(legend.position = "top")
+
+ggsave(here("output/RR_sex_reg_lopoff.png"),w=12,h=5)
+
+
+## country lopoffs
+RRbyC <- DRB[Year==2022,.(RR0=weighted.mean(RR0,tb),
+                          RR17=weighted.mean(RR17,tb),
+                          RR18.5=weighted.mean(RR18.5,tb),
+                          tb=sum(tb)),
+             by=.(iso3,g_whoregion)]
+RRbyC[,RR18.5:=RR18.5/RR0]
+der <- RRbyC[,order(RR18.5,tb)]
+RRbyC$iso3 <- factor(RRbyC$iso3,levels=RRbyC$iso3[der],ordered=TRUE)
+
+
+
+
+ggplot(RRbyC,aes(iso3,RR18.5,size=tb))+
+  geom_point(shape=1)+
+  facet_wrap(~g_whoregion,scales="free")+
+  coord_flip()+
+  theme_linedraw()
+
+ggsave(here("output/RR_country_reg_lopoff.png"),w=12,h=10)
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bmitb
 BMI distributions &amp; TB
+
+
+Notes from call:
+U(18.5,25) as ref pop for RRs and PAFs
+Ref dist (not ideal)
+    conceptual coherence with meaning of ref pop
+    extrapolate kid ref pop?
+may be focus on below rather than PAFs due to protection *
+lopping off lower part of tail in aech country (17 & 18.5)
