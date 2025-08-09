5de047900f859fdbac5c78b4d14b59eb0d0c1619
Pete Dodd <pjd@Petes-Mac-mini.local>
2025-08-09 14:16:34 +0100

    improved table formatting

--- R/2_RRcalculations.R
+++ R/2_RRcalculations.R
@@ -32,10 +32,27 @@ whokeyshort <- rbind(
 
 ssum <- function(x) sqrt(sum(x^2))
 rf <- function(x) {
-  formatC(round(x, digits = -2), digits = 0, format = "d", big.mark = " ")
+  dg <- ifelse(abs(x) > 0.01 & abs(x) < 100, 2, 3)
+  x2 <- signif(x, dg)
+  format(
+    x2,
+    digits = dg,
+    nsmall = 0L,
+    big.mark = ",", #or " "
+    justify = "right",
+    drop0trailing = TRUE,
+    scientific = FALSE
+  )
 }
-brkt <- function(x, y, z) paste0(x, " (", y, " to ", z, ")")
-brkt(1, 2, 3)
+brkt0 <- function(x, y, z) paste0(x, " (", y, " to ", z, ")")
+brkt0(1, 2, 3)
+brkt <- function(x, y, z) {
+  ans <- brkt0(x, y, z)
+  ans <- gsub("\\([[:space:]]+", "\\(", ans)
+  ans <- gsub("to[[:space:]]+", "to ", ans)
+  ans
+}
+
 
 ## Saunders et al linear parameters
 ## risk per one unit increase in BMI was 14.8% (95%CI: 13.3-16.3)
